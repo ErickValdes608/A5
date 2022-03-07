9553d75a62a26a9dcab44855922bbd564449e44f
Gustavo Lara <a17310157@ceti.mx>
2022-03-06 21:23:37 -0800

    Fixed codes

--- client.c
+++ client.c
@@ -16,7 +16,7 @@ int main() {
     struct Data info;
     char command[30] ="mkfifo ";
 
-    char MENU[80] = "Select one: \n 1: Social Service \n 2: Professional Practices\n";
+    char MENU[80] = "\n \n Select one: \n 1: Social Service \n 2: Professional Practices\n";
 
     printf("Set your name: \n");
     scanf("%s", info.name);
@@ -26,21 +26,21 @@ int main() {
     system(command);
 
     while (1) {
-	    printf("%s", MENU);
+	printf("%s", MENU);
         scanf("%d", &info.option);
 
         // Open Server FIFO for write only
-        fdw = open("server", O_WRONLY);
+        fdw = open("server_custom", O_WRONLY);
 
         // Write the input on FIFO
-        // and close it
+        // and close it        
         write(fdw, &info, sizeof(info));
         close(fdw);
 
         // Open Client FIFO for Read only
         fdr = open(info.name, O_RDONLY);
         read(fdr, &info, sizeof(info));
-        printf("Server: %s\n", info.response);
+        printf("Server: %s \n", info.response);
         close(fdr);
     }
     return 0;
--- server.c
+++ server.c
@@ -21,124 +21,126 @@ struct MqData {
 int main() {
     int fdr, fdw;
     //int pid;
-    char MENU[80] = "Waiting for client: \n 1: Social Service \n 2: Professional Practices";
+    char MENU[80] = "\n\nWaiting for client: \n 1: Social Service \n 2: Professional Practices";
     char option[1];
 
     //Open server connection
-    system("mkfifo server");
+    system("mkfifo server_custom");
 
     int waitForProcessFlag = 0;
 
     while(1) {
-        fdr = open("server", O_RDONLY);
+        fdr = open("server_custom", O_RDONLY);
         if(fdr == -1) {
             perror("Fatal error");
             exit(1);
         }
-      if (!waitForProcessFlag)
-      {
-        waitForProcessFlag = 1;
+        
+        if (!waitForProcessFlag)
+	      {
+		waitForProcessFlag = 1;
 
-        printf("%s\n", MENU);
+		printf("%s\n", MENU);
 
-        struct Data info;
-        struct MqData mqInfo;
+		struct Data info;
+		struct MqData mqInfo;
 
-        // Read response from client
-        read(fdr, &info, sizeof(info));
-        // Print the read option
-        printf("Client %s sent: %d\n", info.name, info.option);
+		// Read response from client
+		read(fdr, &info, sizeof(info));
+		// Print the read option
+		printf("Client %s sent: %d\n", info.name, info.option);
+		
+		char content[1024];
+		int mq;
+		int serverId;
+		int processId;
 
-        char content[1024];
+		switch(info.option) {
+		case 1: ;
+		    mq;
+		    key_t key_ss = ftok("ss", 1234);
+		    mq = msgget( key_ss, IPC_CREAT | 0600 | IPC_NOWAIT );
+		    serverId = 1234;
+		    processId = 4321;
+		    
+		    switch(fork()) {
+		        case 0 : ;
+		            //Read and send Social service information from file
+		            int fdpp = open("ss.txt", O_RDONLY);
 
-        switch(info.option) {
-        case 1: ;
-            //int pid;
-            int fd[2];
-            //pid = fork();
-            switch(fork()) {
-                case 0 :
-                    close(fd[0]);
-                    printf("Subprocess pid: %i \n", getpid());
+		            if(fdpp == -1) {
+		                perror("Failed to open file\n");
+		            } else {
+		                read(fdpp, content, sizeof(content));
+		                close(fdpp);
+		                printf("File content: %s\n", content);
+		                mqInfo.id = serverId;
+		                strcpy(mqInfo.message, content);
+		                msgsnd(mq, &mqInfo, sizeof(mqInfo), 0);
+		            }
+		        break;
+		        default:
+		            msgrcv(mq, &mqInfo, sizeof(mqInfo), serverId, 0);
+		            printf("MQ content: %s\n", mqInfo.message);
+		            strcpy(info.response, mqInfo.message);
+		            // Now open in write mode and write
+		            // string taken from MQ.
+		            fdw = open(info.name, O_WRONLY);
 
-                    //Read and send Social Service information from file
-                    int fdss = open("ss.txt", O_RDONLY);
+		            write(fdw, &info, sizeof(info));
+		            close(fdw);
+		            msgctl(mq, IPC_RMID, NULL);
+		            waitForProcessFlag = 0;
+		        break;
+		    }
+		    break;
+		    
+		    
+		case 2: ;
+		    mq;
+		    key_t key = ftok("pp", 123);
+		    mq = msgget( key, IPC_CREAT | 0600 | IPC_NOWAIT );
+		    serverId = 123;
+		    processId = 321;
+		    
+		    switch(fork()) {
+		        case 0 : ;
+		            //Read and send Professional Practices information from file
+		            int fdpp = open("pp.txt", O_RDONLY);
 
-                    if(fdss == -1) {
-                        perror("Failed to open file\n");
-                    } else {
-                        read(fdss, content, sizeof(content));
-                        printf("File content: %s\n", content);
-                        write(fd[1], content, sizeof(content));
-                        close(fd[1]);
-                        close(fdss);
-                    }
-                break;
-                default:
-                    //printf("Subprocess pid: %i \n", getpid());
-                    close(fd[1]);
-                    read(fd[0], content, sizeof(content));
-                    close(fd[0]);
+		            if(fdpp == -1) {
+		                perror("Failed to open file\n");
+		            } else {
+		                read(fdpp, content, sizeof(content));
+		                close(fdpp);
+		                printf("File content: %s\n", content);
+		                mqInfo.id = serverId;
+		                strcpy(mqInfo.message, content);
+		                msgsnd(mq, &mqInfo, sizeof(mqInfo), 0);
+		            }
+		        break;
+		        default:
+		            msgrcv(mq, &mqInfo, sizeof(mqInfo), serverId, 0);
+		            printf("MQ content: %s\n", mqInfo.message);
+		            strcpy(info.response, mqInfo.message);
+		            // Now open in write mode and write
+		            // string taken from MQ.
+		            fdw = open(info.name, O_WRONLY);
 
-                    printf("Fork content: %s\n", content);
-                    strcpy(info.response, content);
-                    
-                    // Now open in write mode the user pipe and write
-                    // the string taken from the fork.
-                    fdw = open(info.name, O_WRONLY);
-
-                    write(fdw, &info, sizeof(info));
-                    close(fdw);
-                    
-                    waitForProcessFlag = 0;
-                break;
-            }
-            break;
-        case 2: ;
-            int mq;
-            key_t key = ftok("pp", 123);
-            mq = msgget( key, IPC_CREAT | 0600 | IPC_NOWAIT );
-            int serverId = 123;
-            int processId = 321;
-            
-            switch(fork()) {
-                case 0 : ;
-                    //Read and send Professional Practices information from file
-                    int fdpp = open("pp.txt", O_RDONLY);
-
-                    if(fdpp == -1) {
-                        perror("Failed to open file\n");
-                    } else {
-                        read(fdpp, content, sizeof(content));
-                        close(fdpp);
-                        printf("File content: %s\n", content);
-                        mqInfo.id = serverId;
-                        strcpy(mqInfo.message, content);
-                        msgsnd(mq, &mqInfo, sizeof(mqInfo), 0);
-                    }
-                break;
-                default:
-                    msgrcv(mq, &mqInfo, sizeof(mqInfo), serverId, 0);
-                    printf("MQ content: %s\n", mqInfo.message);
-                    strcpy(info.response, mqInfo.message);
-                    // Now open in write mode and write
-                    // string taken from MQ.
-                    fdw = open(info.name, O_WRONLY);
-
-                    write(fdw, &info, sizeof(info));
-                    close(fdw);
-                    msgctl(mq, IPC_RMID, NULL);
-                    waitForProcessFlag = 0;
-                break;
-            }
-            break;
-        default:
-            printf("Closing Server\n");
-            return 0;
-            break;
-        }
-      }
-    close(fdr);
-    }
+		            write(fdw, &info, sizeof(info));
+		            close(fdw);
+		            msgctl(mq, IPC_RMID, NULL);
+		            waitForProcessFlag = 0;
+		        break;
+		    }
+		    break;
+		default:
+		    printf("Closing Server\n");
+		    return 0;
+		    break;
+		}
+	      }
+	    close(fdr);
+	    }
     return 0;
 }
